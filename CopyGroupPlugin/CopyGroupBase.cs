215af7f08ea5bcf025584ebcef6f480cc961481f
Ирина Баранова <irina.baranova14@outlook.com>
2024-03-26 22:50:07 +0300

    Доработан плагин, добавлен код для вставки группы объектов в выбранную комнату с вычисленным смещением относительно центра комнаты.

---
+++ CopyGroupPlugin/CopyGroupBase.cs
@@ -0,0 +1,94 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Architecture;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+
+namespace CopyGroupPlugin
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CopyGroupBase : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+                Document doc = uiDoc.Document;
+
+                GroupPickFilter groupPickFilter = new GroupPickFilter();
+                Reference reference = uiDoc.Selection.PickObject(ObjectType.Element, groupPickFilter, "Выберете группу элементов");
+                Element element = doc.GetElement(reference);
+                Group group = element as Group;
+                XYZ groupCenter = GetElementCenter(group);
+                Room room = GetRoomByPoint(doc, groupCenter);
+                XYZ roomCenter = GetElementCenter(room);
+                XYZ offset = groupCenter - roomCenter;
+
+
+                XYZ point = uiDoc.Selection.PickPoint("Выберете точку");
+                Room selectRoom = GetRoomByPoint(doc, point);
+                XYZ centrSelectRoom = GetElementCenter(selectRoom);
+                XYZ groupPosition = centrSelectRoom + offset;
+
+
+                Transaction transaction = new Transaction(doc);
+                transaction.Start("Копирование группы объекта");
+                doc.Create.PlaceGroup(groupPosition, group.GroupType);
+                transaction.Commit();
+            }
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                return Result.Cancelled;
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+
+            }
+
+            return Result.Succeeded;
+        }
+        public XYZ GetElementCenter(Element element)
+        {
+
+            BoundingBoxXYZ bounding = element.get_BoundingBox(null);
+            return (bounding.Max+bounding.Min)/2;
+        }
+
+        public Room GetRoomByPoint(Document doc, XYZ point)
+        {
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+            foreach (Element e in collector)
+            {
+                Room room = e as Room;
+                if (room != null)
+                {
+                    if (room.IsPointInRoom(point))
+                    {
+                        return room;
+                    }
+                }
+            }
+            return null;
+        }
+    }
+    public class GroupPickFilter : ISelectionFilter
+    {
+        public bool AllowElement(Element elem)
+        {
+            if (elem.Category.Id.IntegerValue==(int)BuiltInCategory.OST_IOSModelGroups)
+                return true;
+            else
+                return false;
+        }
+
+        public bool AllowReference(Reference reference, XYZ position)
+        {
+            return false;
+        }
+    }
+}
